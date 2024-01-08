49c607be408e447ee865fff64a6fcbb5db2bf98a
tarunsuthar456 <tarunsuthar9999@gmail.com>
2024-01-08 13:58:33 +0530

    changed like button

--- app/views/layout/footer.php
+++ app/views/layout/footer.php
@@ -37,7 +37,7 @@
       <div class="row mt-3">
         <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
           <h6 class="text-uppercase fw-bold mb-4">
-            <i class="fas fa-gem me-3"></i>Company name
+            <i class="fas fa-gem me-3"></i>Company name 
           </h6>
           <p>
             Here you can use rows and columns to organize your footer content. Lorem ipsum
@@ -45,7 +45,7 @@
           </p>
         </div>
 
-        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+        <!-- <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
           <h6 class="text-uppercase fw-bold mb-4">
             Products
           </h6>
@@ -78,7 +78,7 @@
           <p>
             <a href="#!" class="text-reset">Help</a>
           </p>
-        </div>
+        </div> -->
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
           <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
           <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
